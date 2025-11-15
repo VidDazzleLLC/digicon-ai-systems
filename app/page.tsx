c938458fd38b051596fb397404d328cbfbdb4a05
VidDazzle LLC <connect@viddazzle.com>
2025-11-15 14:36:49 -0600

    Remove framer-motion import from page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 'use client';
 import { useState, useEffect } from 'react';
 import Papa from 'papaparse';
-import { motion } from 'framer-motion';
 
 export default function Home() {
     const [arr, setArr] = useState(20470000)
